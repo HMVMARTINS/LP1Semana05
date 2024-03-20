a168964228f26c06ba8d333cbd5c1e3d09f70a64
HMVMARTINS <hugomvmartins90@gmail.com>
2024-03-20 23:26:41 +0000

    Exercício 2 - Class Stopwatch

---
+++ Chronos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declara e instancia os 2 Cronómetros
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+
+            // Inicia a contagem do tempo em crono1
+            crono1.Start();
+
+            //Pausa o programa durante 0.6 segundos (600 milissegundos):
+            Thread.Sleep(600);
+
+            //Inicia a contagem do tempo em crono2
+            crono2.Start();
+
+            //Pausa o programa durante mais 0.2 segundos
+            Thread.Sleep(200);
+
+            //Para a contagem do tempo nos 2 cronómetros
+            crono1.Stop();
+            crono2.Stop();
+
+            //Mostra no ecrã o tempo decorrido em cada cronómetro, em segundos com 3 casas decimais
+            Console.WriteLine("Tempo decorrido em crono1: " + (crono1.ElapsedMilliseconds / 1000.0).ToString("0.000") + " segundos");
+            Console.WriteLine("Tempo decorrido em crono2: " + (crono2.ElapsedMilliseconds / 1000.0).ToString("0.000") + " segundos");
+        }
+    }
+}
