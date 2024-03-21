4d00177e5c90963476dea354d9cbf3c447bd3d7e
HMVMARTINS <hugomvmartins90@gmail.com>
2024-03-21 23:46:22 +0000

    Exercício 8 - Métodos da classe Enemy

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -1,9 +1,38 @@
 namespace MyGame
 {
-    public Enemy (string name)
-    {
-        this.name = name; // name.oculta this.name
-        health = 100
-        shield = 0;
+    public class Enemy
+    { 
+        private string name;
+        private float shield;
+        private float health;
+
+        public Enemy(string name, float shield, float health)
+        {
+        
+            this.name = name;
+            this.shield = shield;
+            this.health = health;
+        }
+
+        //Método para obter o nome do inimigo
+        public string GetName()
+        {
+            return name;
+        }
+
+        //Método para aplicar dano ao inimigo
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+            float damageStillToInflict = -shield;
+            shield = 0;
+            health -= damageStillToInflict;
+            if (health <0)
+                health = 0;
+            }
+    
+        }
     }
 }
