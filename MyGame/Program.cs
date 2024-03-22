d2ac63d9d5024311763d4274e8e2a71b3eebd608
HMVMARTINS <hugomvmartins90@gmail.com>
2024-03-22 00:38:56 +0000

    Exercício 10 - Fazer uso da classe que criámos

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,27 @@ namespace MyGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length != 1 ||int.TryParse(args[0],out int numberOfEnemies) || numberOfEnemies <=0)
+            {
+                Console.WriteLine("Indica um número válido de inimigos.");
+                return;
+            }
+
+            Enemy[] enemies = new Enemy[numberOfEnemies];
+
+            for (int i = 0; i < numberOfEnemies; i++)
+            {
+                Console.WriteLine($"Nome do inimigo {i + 1}: ");
+                string enemyName = Console.ReadLine();
+
+                enemies[i] = new Enemy(enemyName, 100, 100);
+            }
+
+            Console.WriteLine("Nome Health Shield");
+            foreach (var enemy in enemies)
+            {
+                Console.WriteLine($"{enemy.GetName()} {enemy.GetHealth()} {enemy.GetShield()}"); 
+            }
         }
     }
 }
